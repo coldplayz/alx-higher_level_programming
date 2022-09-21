7a6afe5dc4066c2d6b9e5d5f82a3ff258f63ae8b
greenbel <greenbel.chibuike@gmail.com>
2022-09-21 11:10:07 +0000

    T13: Included missing library.
           new file:   1-listint_len.c
           new file:   13-insert_number.c
           new file:   13-main.c
           new file:   linked_lists.c
           new file:   lists.h

---
+++ 0x01-python-if_else_loops_functions/t13/13-insert_number.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - inserts a new node in a sorted list.
+ * @head: stores the address of the pointer to the first node
+ * @n: integer to initialize new node with
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *temp, *temp2 = NULL;
+	int i, list_len;
+
+	list_len = listint_len(*head);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = number;
+
+	temp = *head;  /* copy head so as not to modify the original */
+	for (i = 0; temp; i++)
+	{
+		if (temp->n >= number)
+		{
+			if (temp == *head)
+			{
+				/* make 'new' the new head */
+				new->next = *head;
+				*head = new;
+				break;
+			}
+			new->next = temp;
+			temp2->next = new;
+			break;
+		}
+
+		temp2 = temp;  /* temp2 saves current struct's pointer */
+		temp = temp->next;  /* temp points to next struct */
+	}
+
+	if (i == list_len)  /* temp == NULL */
+	{
+		/* Insert at list end */
+		/* temp2 is a pointer to the last struct node of the list */
+		temp2->next = new;
+		new->next = NULL;  /* 'new' now last node of list */
+	}
+
+	return (new);
+}
+
+
+
+/**
+ * listint_len - determines the number of elements of the structure listint_s
+ * @h: the head of a structure of type 'struct listint_s'
+ *
+ * Return: the number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+	listint_t *temp;
+
+	temp = (listint_t *)h;
+	for (; temp; temp = temp->next)
+	{
+		n++;
+	}
+
+	return (n);
+}
