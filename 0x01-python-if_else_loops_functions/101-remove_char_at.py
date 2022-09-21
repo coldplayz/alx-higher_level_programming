5ab1f25eb0ec31293a5035ec82c892d39474645a
greenbel <greenbel.chibuike@gmail.com>
2022-09-21 12:58:15 +0000

    T15: more tests.
    modified:   101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,5 +2,7 @@
 
 def remove_char_at(strr, n):
     '''Removes the char at index n in strr'''
-    cpy = strr[: n] + strr[n+1 :]
-    return cpy
+    if n >= 0:
+        cpy = strr[:n] + strr[n+1:]
+        return cpy
+    return strr
