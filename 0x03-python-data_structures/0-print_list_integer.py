1d354316994ed8f682d2a1c388759d4da81d8cf4
greenbel <greenbel.chibuike@gmail.com>
2022-09-23 11:58:07 +0000

    T0: Initial commit.
           new file:   0-main.py
           new file:   0-print_list_integer.py
           new file:   README.md
           new file:   __pycache__/0-print_list_integer.cpython-38.pyc
           new file:   pyheader
           new file:   shheader

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_list_integer(my_list=[]):
+    '''Prints integers in my_list'''
+
+    for i in my_list:
+        print("{:d}".format(i))
+    if len(my_list) == 0:
+        print()
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,5 @@
+# Project on List and Tuple Data Structures
+
+* 0-print_list_integer.py: print integers in a list.
+
+
