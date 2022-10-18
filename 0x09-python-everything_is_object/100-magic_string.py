bd14f0ed05310ffb448cab8661c3c081cfa37ede
greenbel <greenbel.chibuike@gmail.com>
2022-10-18 18:33:50 +0100

    T30-0x09: included #! line.

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
+#!/usr/bin/python3
 def magic_string():
-    magic_string.c = getattr(magic_string, 'c', 0) + 1
-    s = "BestSchool" if magic_string.c == 1 else "BestSchool, BestSchool"
+    magic_string.c = getattr(magic_string, 'c', 0) + 1; s = "BestSchool" if magic_string.c == 1 else "BestSchool, BestSchool"
     return s + ", BestSchool" * (magic_string.c - 2)
