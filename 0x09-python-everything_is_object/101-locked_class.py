72e3b8479c644a31bad1ac502bcfa262bea684b9
greenbel <greenbel.chibuike@gmail.com>
2022-10-19 10:18:32 +0100

    T30-0x09

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,6 +11,13 @@ class LockedClass:
     def __setattr__(self, key, value):
         if key != 'first_name':
             raise AttributeError(
-                    f"'{self.__class__.__name__}'" +
+                    f"'LockedClass'" +
                     f" object has no attribute '{key}'")
         object.__setattr__(self, key, value)
+
+    def __getattribute__(self, key):
+        if key != 'first_name':
+            raise AttributeError(
+                    f"'LockedClass'" +
+                    f" object has no attribute '{key}'")
+        object.__getattribute__(self, key)
