2e020ca389bc9b07221cb8f20d7d89fe7f39e6fd
Greenbel <greenbel.chibuike@gmail.com>
2022-10-24 18:58:36 +0100

    T1-0x0A

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-'''Module for Mylist class.
+'''
+Module for Mylist class.
 '''
 
 
 class Mylist(list):
-    '''Subclass of list for sorted print.
+    '''
+    Subclass of list for sorted print.
     '''
 
     def print_sorted(self):
-        '''Prints a sorted list.
+        '''
+        Prints a sorted list.
         '''
 
         print(sorted(self))
