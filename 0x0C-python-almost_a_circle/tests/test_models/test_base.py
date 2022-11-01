a22dfba39683c5fc8deb6d0bd7e7fcb3ca86933f
Greenbel <greenbel.chibuike@gmail.com>
2022-11-01 17:00:15 +0100

    0x0C: added more tests.

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -9,16 +9,14 @@ class TestBase(unittest.TestCase):
     '''Class for testing proper id allocation of Base instances.
     '''
 
-    @classmethod
-    def setUpClass(cls):
-        cls.Base2 = type("Base2", (Base,), dict(Base.__dict__))
-
     def setUp(self):
-        self.b1 = self.__class__.Base2()
-        self.b2 = self.__class__.Base2()
-        self.b3 = self.__class__.Base2()
-        self.b4 = self.__class__.Base2(12)
-        self.b5 = self.__class__.Base2()
+        Base2 = type("Base2", (Base,), dict(Base.__dict__))
+        Base2.reset_nb()
+        self.b1 = Base2()
+        self.b2 = Base2()
+        self.b3 = Base2()
+        self.b4 = Base2(12)
+        self.b5 = Base2()
 
     def tearDown(self):
         del self.b1
@@ -34,9 +32,23 @@ class TestBase(unittest.TestCase):
         self.assertEqual(self.b3.id, 3)
         self.assertEqual(self.b4.id, 12)
         self.assertEqual(self.b5.id, 4)
-        self.assertRaises(TypeError, self.__class__.Base2, "5")
+        self.assertRaises(TypeError, Base, "5")
 
     def test_private(self):
         '''Test for inaccessibilty of private attribute.'''
         with self.assertRaises(AttributeError):
-            self.__class__.Base2.__nb_objects
+            self.__class__.Base.__nb_objects
+
+    def test_to_json_string(self):
+        '''Test for correct conversion to JSON
+        '''
+        self.assertEqual(self.b1.to_json_string(None), '[]')
+        self.assertEqual(self.b2.to_json_string([]), '[]')
+        self.assertEqual(self.b1.to_json_string([{'id': 12}]), '[{"id": 12}]')
+
+    def test_from_json_string(self):
+        '''Test for correct conversion from JSON
+        '''
+        self.assertEqual(self.b1.from_json_string(None), [])
+        self.assertEqual(self.b2.from_json_string('[]'), [])
+        self.assertEqual(self.b3.from_json_string('[{"id": 89}]'), [{"id": 89}])
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,6 +5,9 @@ import unittest
 from models.rectangle import Rectangle
 from models.base import Base
 from unittest.mock import patch
+import io
+import sys
+import json
 
 
 class TestRectangle(unittest.TestCase):
@@ -16,6 +19,7 @@ class TestRectangle(unittest.TestCase):
         self.r1 = Rectangle(10, 2)
         self.r2 = Rectangle(2, 20)
         self.r3 = Rectangle(10, 2, 0, 0, 12)
+        self.r4 = Rectangle(10, 2, 3)
 
     def test_attr(self):
         '''Tests for correct attribute assignment.'''
@@ -38,6 +42,10 @@ class TestRectangle(unittest.TestCase):
             Rectangle(-10, 6)
         with self.assertRaises(ValueError):
             Rectangle(-12, -6)
+        self.assertRaises(ValueError, Rectangle, 0, 1)
+        self.assertRaises(ValueError, Rectangle, 1, 0)
+        self.assertRaises(ValueError, Rectangle, 2, 1, -3)
+        self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
 
     def test_type_validation(self):
         '''Tests for proper input type validation
@@ -57,6 +65,9 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(12, 6.5)
         self.assertRaises(TypeError, Rectangle, float("nan"), 6)
+        self.assertRaises(TypeError, Rectangle, 1,
+ 2, "3")
+        self.assertRaises(TypeError, Rectangle, 1, 2, 3, "4")
 
     def test_area(self):
         '''Tests for value of area
@@ -68,7 +79,7 @@ class TestRectangle(unittest.TestCase):
     def test_str(self):
         '''Tests the string representation
         '''
-        self.assertEqual(str(self.r3), "[Rectangle] (12) 0/0 10/2")
+        self.assertEqual(str(self.r3), "[Rectangle] (12) 0/0 - 10/2")
 
     def test_update(self):
         '''Tests for proper update
@@ -83,3 +94,61 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.r3.y, 37)
         self.assertEqual(self.r3.id, 100)
         self.assertRaises(TypeError, Rectangle, "50")
+
+    def test_display(self):
+        '''Tests  display
+        '''
+        captured_out = io.StringIO()  # create a StringIO object
+        sys.stdout = captured_out  # redirect stdout to the StringIO object
+        self.r1.display()  # produce the output to capture
+        self.assertEqual(captured_out.getvalue(), "##########\n##########\n")
+        sys.stdout = sys.__stdout__  # reset to default terminal file object
+        self.assertEqual(self.r4.display(), None)
+        self.assertEqual(self.r3.display(), None)
+
+    def test_to_dictionary(self):
+        '''Tests for proper conversion of Rectangle object to dictionary
+        '''
+        self.assertEqual(self.r1.to_dictionary(), dict(width=10, height=2, x=0, y=0, id=1))
+        self.assertEqual(self.r4.to_dictionary(), dict(width=10, height=2, x=3, y=0, id=3))
+
+    def test_create(self):
+        '''Test for proper creation of Rectangle instances
+        '''
+        self.assertIs(type(Rectangle.create(**{"id": 89})), Rectangle)
+        self.assertIs(type(Rectangle.create(**{"id": 89, "width": 1})), Rectangle)
+        self.assertIs(type(Rectangle.create(**{"id": 89, "width": 1, "height": 2})), Rectangle)
+        self.assertIs(type(Rectangle.create(**{"id": 89, "width": 1, "height": 2, "x": 3})), Rectangle)
+        self.assertIs(type(Rectangle.create(**{"id": 89, "width": 1, "height": 2, "x": 3, "y": 4})), Rectangle)
+        self.assertEqual(Rectangle.create(**{"id": 89, "width": 1, "height": 2, "x": 3, "y": 4}).height, 2)
+
+    def test_save_to_file(self):
+        '''Tests for success of state save
+        '''
+        Rectangle.save_to_file([self.r1])
+        try:
+            with open("Rectangle.json", 'r', encoding='utf-8') as fin:
+                s = fin.read()
+                self.assertEqual(json.loads(s), [{"width": 10, "height": 2, "x": 0, "y": 0, "id": 1}])
+        except FileNotFoundError:
+            self.fail("state not saved to json file")
+        Rectangle.save_to_file([])
+        try:
+            with open("Rectangle.json", 'r', encoding='utf-8') as fin:
+                s = fin.read()
+                self.assertEqual(json.loads(s), [])
+        except FileNotFoundError:
+            self.fail("state not saved to json file")
+        Rectangle.save_to_file(None)
+        try:
+            with open("Rectangle.json", 'r', encoding='utf-8') as fin:
+                s = fin.read()
+                self.assertEqual(json.loads(s), [])
+        except FileNotFoundError:
+            self.fail("state not saved to json file")
+
+    def test_load_from_file(self):
+        '''Tests for correct deserialization from json file
+        '''
+        inst = Rectangle.load_from_file()
+        self.assertIs(type(inst), list)
