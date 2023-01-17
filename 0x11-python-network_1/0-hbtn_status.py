b591b68831ab56315c2a8cd5ddfc55c2c016e714
greenbel <greenbel.chibuike@gmail.com>
2023-01-17 14:29:38 +0000

    T0-0x11: initial commit.Z

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' Fetches https://alx-intranet.hbtn.io/status, with urllib.
+'''
+from urllib import request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with request.urlopen(url) as response:
+    html = response.read()
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,5 @@
+# Python - Networking_1
+
+* 0-hbtn_status.py: fetches https://alx-intranet.hbtn.io/status with urllib
+
+
