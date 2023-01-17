f4800a2c5cedbbb86e4c9154a53282e0fa511b3e
Greenbel <greenbel.chibuike@gmail.com>
2023-01-17 16:19:31 +0100

    T0-0x11: fixed encoding display.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,21 +3,22 @@
 '''
 from urllib import request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with request.urlopen(url) as response:
-    content = response.read()
+    with request.urlopen(url) as response:
+        content = response.read()
 
-    # Parse out the charset from `Content-Type` response header
-    contentType = response.getheader('Content-Type')
-    typeList = contentType.split(";")
-    typeList = [x.strip() for x in typeList]
-    for x in typeList:
-        if x.startswith('charset'):
-            charset = x.split("=")[1]
+        # Parse out the charset from `Content-Type` response header
+        contentType = response.getheader('Content-Type')
+        typeList = contentType.split(";")
+        typeList = [x.strip() for x in typeList]
+        for x in typeList:
+            if x.startswith('charset'):
+                charset = x.split("=")[1]
 
-    # Display formated output
-    print('Body response:')
-    print(f'\t- type: {type(content)}')
-    print(f'\t- content: {content}')
-    print(f'\t- {charset} content: OK')
+        # Display formated output
+        print('Body response:')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        print(f'\t- utf8 content: {content.decode("utf-8")}')
