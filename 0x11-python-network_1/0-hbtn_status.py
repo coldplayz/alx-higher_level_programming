a74f8c0ba4f05bb58a491cccbd0e8aa85e8dc1a0
Greenbel <greenbel.chibuike@gmail.com>
2023-01-17 16:07:33 +0100

    T0-0x11.

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,4 +6,18 @@ from urllib import request
 url = 'https://alx-intranet.hbtn.io/status'
 
 with request.urlopen(url) as response:
-    html = response.read()
+    content = response.read()
+
+    # Parse out the charset from `Content-Type` response header
+    contentType = response.getheader('Content-Type')
+    typeList = contentType.split(";")
+    typeList = [x.strip() for x in typeList]
+    for x in typeList:
+        if x.startswith('charset'):
+            charset = x.split("=")[1]
+
+    # Display formated output
+    print('Body response:')
+    print(f'\t- type: {type(content)}')
+    print(f'\t- content: {content}')
+    print(f'\t- {charset} content: OK')
