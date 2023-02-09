7fffd07ad04d3dcbdd9dbbe265de20f3d86ffcbf
Greenbel <greenbel.chibuike@gmail.com>
2023-02-09 19:12:31 +0100

    T0-0x14: changed from promises to callback async

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,16 +1,13 @@
 #!/usr/bin/node
 // reads and prints the contents of a file.
 
-const fs = require('fs').promises;
+const fs = require('fs');
 const filePath = process.argv[2];
 
-async function readf (filePath) {
-  try {
-    const data = await fs.readFile(filePath, { encoding: 'utf8' });
-    console.log(data.toString());
-  } catch (error) {
-    console.error(error);
+fs.readFile(filePath, { encoding: 'utf8' }, (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
   }
-}
-
-readf(filePath);
+});
